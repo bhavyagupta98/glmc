82c28428f104968058e8471b0b8e2f21916d37ba
Bhavya <Bhavya@admins-MacBook-Pro.local>
2018-09-13 16:35:44 +0530

    mat4 added (inverse and detrminant functions to be created)

---
+++ mat4.c
@@ -0,0 +1,119 @@
+#include "glmc.h"
+
+void glmc_mat4_add(mat4 dest,mat4 src_a,mat4 src_b){
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[i][j] = src_a[i][j] + src_a[i][j];
+		}
+	}
+}
+
+void glmc_mat4_sub(mat4 dest,mat4 src_a,mat4 src_b){
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[i][j] = src_a[i][j] - src_a[i][j];
+		}
+	}
+}
+
+void glmc_mat4_mul(mat4 dest,mat4 src_a,mat4 src_b){
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[i][j] = 0;
+			for (int k=0;k<4;k++){
+				dest[i][j] += src_a[i][k] * src_a[k][j];
+			}
+		}
+	}
+}
+
+void glmc_mat4_div(mat4 dest,mat4 src_a,mat4 src_b){
+	mat4 inv;
+	glmc_mat4_inverse(inv,src_b);
+	glmc_mat4_mul(dest,src_a,inv);
+}
+
+void glmc_mat4_mul_scalar(mat4 dest,mat4 src_a,float src){
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[i][j] = src_a[i][j]*src;
+		}
+	}
+}
+
+void glmc_mat4_div_scalar(mat4 dest,mat4 src_a,float src){
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[i][j] = src_a[i][j]/src;
+		}
+	}
+}
+
+void glmc_mat4_normalize(mat4 dest,mat4 src_a){
+	float magnitude = 0;
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			magnitude += src_a[i][j]*src_a[i][j];
+		}
+	}
+	magnitude = sqrt(magnitude);
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[i][j] = src_a[i][j]/magnitude;
+		}
+	}
+}
+
+void glmc_mat4_madd(mat4 dest,mat4 src_a,mat4 src_b){
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[i][j] += (src_a[i][j]*src_b[i][j]);
+		}
+	}
+}
+
+void glmc_mat4_msub(mat4 dest,mat4 src_a,mat4 src_b){
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[i][j] -= (src_a[i][j]*src_b[i][j]);
+		}
+	}
+}
+/*
+float glmc_mat4_discriminant(mat4 dest){
+	float disc = 0;
+	for(int i=0;i<4;i++){
+		int j = i+1;
+		if(i==1){
+			disc += dest[0][i]*(dest[1][(j+1)%4]*dest[2][j%4]);
+		}
+		else {
+			disc += -(dest[0][i]*(dest[1][(j+1)%4]*dest[2][j%4]));
+		}
+	}
+	return disc;
+}
+
+void glmc_mat4_inverse(mat4 dest,mat4 src){
+	float det = glmc_mat4_discriminant(src);
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			if ((i+j)%2==0){
+				dest[j][i] = ((src[(i+1)%4][(j+1)%4]*src[(i+2)%4][(j+2)%4])-(src[(i+2)%4][(j+1)%4]*src[(i+1)%4][(j+2)%4]))/det;
+			}
+			else{
+				dest[j][i] = -((src[(i+1)%4][(j+1)%4]*src[(i+2)%4][(j+2)%4])-(src[(i+2)%4][(j+1)%4]*src[(i+1)%4][(j+2)%4]))/det;
+			}
+		}
+	}
+}
+*/	
+
+
+void glmc_mat4_transpose(mat4 dest,mat4 src){
+	for (int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			dest[j][i] = src[i][j];
+		}
+	}
+}
