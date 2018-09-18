e75c6e1a122551da053dee9dedaa1ebb37f0d5cc
Bhavya <Bhavya@admins-MacBook-Pro.local>
2018-09-18 16:14:30 +0530

    mat2 corrected

--- mat2.c
+++ mat2.c
@@ -1,30 +1,24 @@
 #include "glmc.h"
 
 void glmc_mat2_add(mat2 dest,mat2 src_a,mat2 src_b){
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[i][j] = src_a[i][j] + src_a[i][j];
-		}
-	}
+	dest[0][0] = src_a[0][0] + src_b[0][0];
+	dest[0][1] = src_a[0][1] + src_b[0][1];
+	dest[1][0] = src_a[1][0] + src_b[1][0];
+	dest[1][1] = src_a[1][1] + src_b[1][1];
 }
 
 void glmc_mat2_sub(mat2 dest,mat2 src_a,mat2 src_b){
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[i][j] = src_a[i][j] - src_a[i][j];
-		}
-	}
+	dest[0][0] = src_a[0][0] - src_b[0][0];
+	dest[0][1] = src_a[0][1] - src_b[0][1];
+	dest[1][0] = src_a[1][0] - src_b[1][0];
+	dest[1][1] = src_a[1][1] - src_b[1][1];
 }
 
 void glmc_mat2_mul(mat2 dest,mat2 src_a,mat2 src_b){
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[i][j] = 0;
-			for (int k=0;k<2;k++){
-				dest[i][j] += src_a[i][k] * src_a[k][j];
-			}
-		}
-	}
+	dest[0][0] = (src_a[0][0]*src_b[0][0]) + (src_a[0][1]*src_b[1][0]);
+	dest[0][1] = (src_a[0][0]*src_b[0][1]) + (src_a[0][1]*src_b[1][1]);
+	dest[1][0] = (src_a[1][0]*src_b[0][0]) + (src_a[1][1]*src_b[1][0]);
+	dest[1][1] = (src_a[1][0]*src_b[0][1]) + (src_a[1][1]*src_b[1][1]);
 }
 
 void glmc_mat2_div(mat2 dest,mat2 src_a,mat2 src_b){
@@ -34,50 +28,42 @@ void glmc_mat2_div(mat2 dest,mat2 src_a,mat2 src_b){
 }
 
 void glmc_mat2_mul_scalar(mat2 dest,mat2 src_a,float src){
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[i][j] = src_a[i][j]*src;
-		}
-	}
+	dest[0][0] = src_a[0][0]*src;
+	dest[0][1] = src_a[0][1]*src;
+	dest[1][0] = src_a[1][0]*src;
+	dest[1][1] = src_a[1][1]*src;
 }
 
 void glmc_mat2_div_scalar(mat2 dest,mat2 src_a,float src){
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[i][j] = src_a[i][j]/src;
-		}
-	}
+	dest[0][0] = src_a[0][0]/src;
+	dest[0][1] = src_a[0][1]/src;
+	dest[1][0] = src_a[1][0]/src;
+	dest[1][1] = src_a[1][1]/src;
 }
 
 void glmc_mat2_normalize(mat2 dest,mat2 src_a){
 	float magnitude = 0;
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			magnitude += src_a[i][j]*src_a[i][j];
-		}
-	}
+	magnitude += src_a[0][0]*src_a[0][0];
+	magnitude += src_a[0][1]*src_a[0][1];
+	magnitude += src_a[1][0]*src_a[1][0];
+	magnitude += src_a[1][1]*src_a[1][1];
+	
 	magnitude = sqrt(magnitude);
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[i][j] = src_a[i][j]/magnitude;
-		}
-	}
+	glmc_mat2_div_scalar(dest,src_a,magnitude);
 }
 
 void glmc_mat2_madd(mat2 dest,mat2 src_a,mat2 src_b){
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[i][j] += (src_a[i][j]*src_b[i][j]);
-		}
-	}
+	dest[0][0] += (src_a[0][0]*src_b[0][0]) + (src_a[0][1]*src_b[1][0]);
+	dest[0][1] += (src_a[0][0]*src_b[0][1]) + (src_a[0][1]*src_b[1][1]);
+	dest[1][0] += (src_a[1][0]*src_b[0][0]) + (src_a[1][1]*src_b[1][0]);
+	dest[1][1] += (src_a[1][0]*src_b[0][1]) + (src_a[1][1]*src_b[1][1]);
 }
 
 void glmc_mat2_msub(mat2 dest,mat2 src_a,mat2 src_b){
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[i][j] -= (src_a[i][j]*src_b[i][j]);
-		}
-	}
+	dest[0][0] -= (src_a[0][0]*src_b[0][0]) + (src_a[0][1]*src_b[1][0]);
+	dest[0][1] -= (src_a[0][0]*src_b[0][1]) + (src_a[0][1]*src_b[1][1]);
+	dest[1][0] -= (src_a[1][0]*src_b[0][0]) + (src_a[1][1]*src_b[1][0]);
+	dest[1][1] -= (src_a[1][0]*src_b[0][1]) + (src_a[1][1]*src_b[1][1]);
 }
 
 float glmc_mat2_discriminant(mat2 dest){
@@ -93,9 +79,8 @@ void glmc_mat2_inverse(mat2 dest,mat2 src){
 }
 
 void glmc_mat2_transpose(mat2 dest,mat2 src){
-	for (int i=0;i<2;i++){
-		for (int j=0;j<2;j++){
-			dest[j][i] = src[i][j];
-		}
-	}
+	dest[0][0] = src[0][0];
+	dest[0][1] = src[1][0];
+	dest[1][0] = src[0][1];
+	dest[1][1] = src[1][1];
 }
